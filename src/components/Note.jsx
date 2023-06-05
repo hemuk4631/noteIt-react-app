bbd2c94a4339a3bee80e4380ae3b0fc2c7fcfa51
hemu kumar <hemuk4631@gmail.com>
2023-06-05 09:29:28 +0530

    conflicts resolved

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -5,7 +5,6 @@ function Note({ id, title, content, timestamp, onDelete }) {
   const formattedTimestamp = new Date(timestamp).toLocaleString();
 =======
 function Note({ id, title, content,timestamp, onDelete }) {
->>>>>>> experimental-2
 
   //adding timestamp to note component.
   const formattedTimeStamp = new Date(timestamp).toLocaleString();
@@ -18,12 +17,8 @@ function Note({ id, title, content,timestamp, onDelete }) {
     <div className="note bg-success text-white p-3 rounded shadow">
       <h1 className="text-dark mb-3">{title}</h1>
       <p className="text-white mb-3">{content}</p>
-      <p className="text-white">
-<<<<<<< HEAD
-        Saved on: {formattedTimestamp}
-=======
+      <p className="timestamp text-white">
         Saved on: {formattedTimeStamp}
->>>>>>> experimental-2
       </p>
       <button onClick={handleDelete} className="btn">
         <i className="fa-solid fa-trash"></i>
