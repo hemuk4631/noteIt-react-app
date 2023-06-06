a0f36171a2ca4d92403054c8c4eb48270817626d
hemu kumar <hemuk4631@gmail.com>
2023-06-06 06:12:09 +0530

    backend url added to frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,9 +3,9 @@ import { createRoot } from "react-dom/client";
 import App from "./components/App";
 
 createRoot(document.getElementById("root")).render(
-  <React.StrictMode> 
+  
     <App />
-  </React.StrictMode>
+  
 );
 // <React.StrictMode> is For detecting the potential issues and provide warnings to the browser console.
 
